3bf07d033d53b5a480e3415c50c6b932d690a032
simbamike <44067292+simbamike@users.noreply.github.com>
2021-03-13 20:38:55 -0700

    Clean up
    Code clean up

--- askingprogram.py
+++ askingprogram.py
@@ -7,6 +7,31 @@ def read_questions(filename):
                 question, answer = line.split('=')
                 answers[question.strip()] = answer.strip()
     return answers
-read_questions('/Users/michael/Documents/questions.txt')
-{'text displaying function': 'print', 'text asking function': 'input'}
-print(answers)
+
+def ask_questions(answers):
+    correct = []
+    wrong = []
+    for question, answer in answers.items():
+        if input(question + ' = ').strip() == answer:
+            print("Correct!")
+            correct.append(question)
+        else:
+            print("Wrong! The correct answer is %s." % answer)
+            wrong.append(question)
+    return (correct, wrong)
+
+def stats(correct, wrong, answers):
+    print("\n**** STATS ****\n")
+    print("You answered", len(correct), "questions correctly and",len(wrong), "questions wrong.")
+    if wrong:
+        print("These would have been the correct answers:")
+        for question in wrong:
+            print(' ', question, '=', answers[question])
+def main():
+    filename = '/Users/michael/Documents/Github/python-works/questions.txt'
+    answers = read_questions(filename)
+    correct, wrong = ask_questions(answers)
+    stats(correct,wrong,answers)
+
+if __name__ == '__main__':
+    main()
